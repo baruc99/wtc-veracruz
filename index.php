685587a76c5a938613c3a3e36b1917f3dadd1b96
Nestor Villa <nvilla@iMac-de-Nestor.local>
2023-05-09 14:49:28 -0600

    ultimos cambios del banner

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php get_header(); ?>
 
-<section style="height: 600px; background: gray;">
-   <img src="<?php echo get_template_directory_uri();?>/images/wtcfoto.png" class="fullsize" alt="banner">
-   
+<section class="position-relative">
+   <!-- <img src="<?php echo get_template_directory_uri();?>/images/wtcfoto.png" class="fullsize" alt="banner"> -->
+   <div class="img-banner position-absolute top-0 start-0"></div>
+   <div class="container">
+      <div class="titulofotografiawtc">
+         <p>El lugar donde 
+         todas las expectativas
+         se hacen realidad
+         </p>
+      </div>
+   </div>
 </section>
 
 
