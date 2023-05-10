433b6683bfe3a2cb341228db133577614d96f5f1
BARUC ALBERTO GOMEZ SANCHEZ <bagomez@veracruz.gob.mx>
2023-05-09 18:50:14 -0600

    se agrego las cards y el titulo

--- index.php
+++ index.php
@@ -1,18 +1,40 @@
 <?php get_header(); ?>
 
-<section class="position-relative">
-   <!-- <img src="<?php echo get_template_directory_uri();?>/images/wtcfoto.png" class="fullsize" alt="banner"> -->
-   <div class="img-banner position-absolute top-0 start-0"></div>
-   <div class="container">
-      <div class="titulofotografiawtc">
-         <p>El lugar donde 
-         todas las expectativas
-         se hacen realidad
-         </p>
-      </div>
+<section class="fotografiawtc">
+   <!-- <img src="<?php //echo get_template_directory_uri();?>/images/wtcfoto.png" class="fullsize" alt="banner"> -->
+   <div class="img-banner"></div>
+   <div class="positionTitleBanner">
+         <p> El lugar donde <br></p>
+         <p>todas las expectativas<br></p>
+         <p><span>se hacen realidad</span> </p>
+      
    </div>
 </section>
 
+<section style="height: 320px; background: #e5e5e5;">
+
+   <div class="container">
+      <div class="row">
+         <div class="col-4">
+            <div class="card-index">
+               <h3>Centro de Negocios</h3>
+               <p>Un área común, que ofrece espacios físicos de trabajo y salas de reuniones diseñadas para sus necesidades de negocios</p>
+            </div>
+         </div>
+         <div class="col-4">
+            <h3>Áreas de Cocina</h3>
+            <p>Con la finalidad de dar un servicio integral, se ponen a disposición 3 cocinas.</p>
+         </div>
+         <div class="col-4">
+            <h3>Mesa de Servicios</h3>
+            <p>Podrá contratar los productos y servicios que se adapten a sus necesidades durante el montaje y desmontaje del evento</p>
+         </div>
+      </div>
+   </div>
+
+   
+
+</section>
 
 
 <?php get_footer(); ?>
