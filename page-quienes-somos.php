a40ef5364d6ca10d30ce82ca38efa455ff9506ad
BARUC ALBERTO GOMEZ SANCHEZ <bagomez@veracruz.gob.mx>
2023-05-22 11:42:41 -0600

    quienes somos

--- page-quienes-somos.php
+++ page-quienes-somos.php
@@ -3,11 +3,15 @@
 <div class="wrapper-quienes-somos">
     <div class="container">
         <div class="row">
-            <div class="col-5" style="padding: 5rem 3rem 3rem 3rem;">
-                <img src="<?php echo get_template_directory_uri();?>/images/world-trade-quienes-somos.png" alt="">
+            <div class="col-6" style=" display: flex; align-items: center; justify-content: center;">
+                <img src="<?php echo get_template_directory_uri();?>/images/world-trade-quienes-somos.png" alt="world trade center" style="height: 400px;">
             </div>
-            <div class="col-7" style="padding: 3rem 2rem 3rem 3rem">
-                <h3 class="title">¿QUIÉNES SÓMOS?</h3>
+            <div class="col-6">
+                <div class="d-flex-9 mt-5">
+                    <img class="hornaments-quienes-somos" src="<?php echo get_template_directory_uri();?>/images/hornament.png">
+                    <h3 class="title">¿QUIÉNES SÓMOS?</h3>
+                    <img class="hornaments-quienes-somos" src="<?php echo get_template_directory_uri();?>/images/hornament.png">
+                </div>              
                 <p>World Trade Center Veracruz (WTC Veracruz), afiliado a la World Trade Center Asociation (WTCA) como el tercero en operación en la República Mexicana forma parte de la cadena promotora de negocios más importantes del mundo integrada por 300 WTC ́S con presencia en 100 países.</p>
                 <p>WTC Veracruz fue creado para satisfacer todas las expectativas y necesidades de clientes, locales nacionales y extranjeros para realizar reuniones de trabajo, congresos, convenciones, exposiciones y negocios en tierras veracruzanas.</p>
                 <p>Diseñado y equipado conforme a los estándares internaciones, respaldado por la Asociación Mexicana de Recintos Feriales (AMEREF), es el único recinto en el país que ofrece en un mismo nivel, capacidad para dar solución óptima a todo tipo de eventos en un total de 20,589 m2.</p>
@@ -18,4 +22,6 @@
 </div>
 
 <?php get_template_part('eslogan'); ?>
+
+
 <?php get_footer(); ?>
