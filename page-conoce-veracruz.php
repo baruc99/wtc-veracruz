47abf49c4b2336b65c02e056fe6dd222b4506b6f
barucgs <barucgs@gmail.com>
2023-05-22 23:56:22 -0600

    cambios del 22 de mayo

--- page-conoce-veracruz.php
+++ page-conoce-veracruz.php
@@ -1,4 +1,33 @@
 <?php get_header(); ?>
-
+ <div class="wrapper-conoce-veracruz textura">
+    <div class="container">
+        <div class="row">
+            <div class="col align-Center-title-conoce-veracruz">
+                <img class="hornaments-quienes-somos pr-3" src="<?php echo get_template_directory_uri();?>/images/hornament.png">
+                <h3 class="title-conoce-veracruz"> <span>CONOCE</span> VERACRUZ</h3>
+                <img class="hornaments-quienes-somos pl-3" src="<?php echo get_template_directory_uri();?>/images/hornament.png">
+            </div>
+        </div>
+    </div>
+    <div class="conoce-veracruz">
+        <div class="box">
+           <img class="full-size" src="<?php echo get_template_directory_uri();?>/images/conoce-veracruz/jarochas.png" alt="">
+        </div>
+        <div class="box">
+            <div class="mosaico-conoce-veracruz">
+                <div class="box-m">
+                    <div class="columna-hoteles">
+                        <div class="columna-hoteles" style="background: blue;"></div>
+                        <div class="columna-hoteles" style="background: green;"></div>
+                    </div>
+                </div>
+                <div class="box-m">
+                        <div class="columna-hoteles" style="background: red;"></div>
+                        <div class="columna-hoteles" style="background: yellow;"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+ </div>
 
 <?php get_footer(); ?>
