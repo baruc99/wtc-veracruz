43791123687bdccd0094cad220a9c64e44549cbd
Nestor Villa <nvilla@iMac-de-Nestor.local>
2023-05-24 17:51:36 -0600

    cambios nestor 24 de mayo

--- page-eventos.php
+++ page-eventos.php
@@ -1,5 +1,55 @@
 <?php get_header(); ?>
 
-<h1>Eventos</h1>
+<section class="wrapper-eventos">
+    <div class="img-banner-eventos"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col altuta-texto-eventos texto-eventos">
+                <h3 class="title-seccion-eventos">CARTELERA</h3>
+                <p>World Trade Center Veracruz está lleno de eventos, abiertos al público o para un segmento especíﬁco.<br></p>
+                <p><span>¡NUESTRA CARTELERA TIENE ALGO PARA TI!</span></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-4 agenda-evento p-3">
+                <div class="card-eventos"></div>
+            </div>
+            <div class="col-4 agenda-evento p-3">
+                <div class="card-eventos"></div>
+            </div>
+            <div class="col-4 agenda-evento p-3">
+                <div class="card-eventos"></div>
+            </div>
+            <div class="col-4 agenda-evento p-3">
+                <div class="card-eventos"></div>
+            </div>
+            <div class="col-4 agenda-evento p-3">
+                <div class="card-eventos"></div>
+            </div>
+            <div class="col-4 agenda-evento p-3">
+                <div class="card-eventos"></div>
+            </div>
+            <div class="col-4 agenda-evento p-3">
+                <div class="card-eventos"></div>
+            </div>
+            <div class="col-4 agenda-evento p-3">
+                <div class="card-eventos"></div>
+            </div>
+            <div class="col-4 agenda-evento p-3">
+                <div class="card-eventos"></div>
+            </div>
+            <div class="col-4 agenda-evento p-3">
+                <div class="card-eventos"></div>
+            </div>
+            <div class="col-4 agenda-evento p-3">
+                <div class="card-eventos"></div>
+            </div>
+            <div class="col-4 agenda-evento p-3">
+                <div class="card-eventos"></div>
+            </div>
+        </div>
+    </div>
+</section>
+
 
 <?php get_footer(); ?>
