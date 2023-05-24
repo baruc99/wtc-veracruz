567d9123d38ddf0f6540fa40f776160ea556a2ba
barucgs <barucgs@gmail.com>
2023-05-24 01:08:17 -0600

    se termino la parte de instalaciones

--- page-instalaciones.php
+++ page-instalaciones.php
@@ -9,27 +9,38 @@
         </div>
     </div>
     <div class="instalaciones">
-        <div class="box-instalaciones">
+        <div class="box-instalaciones" style="z-index: 0;position: relative;">
             <img class="full-size" src="<?php echo get_template_directory_uri();?>/images/instalaciones/salonTajin.png" alt="SALONES TAJÍN">
+            <div class="flecha-izquierda centrar-fecha-izquierda"></div>
         </div>
-        <div class="box-instalaciones" style=" background: #EC945E"></div>
-        <div class="box-instalaciones">
-            <img src="<?php echo get_template_directory_uri();?>/images/instalaciones/areaComun.png" alt="ÁREAS COMUNES">
+        <div class="box-instalaciones centrar-texto" style=" background: #EC945E">
+            <p>SALONES TAJÍN</p>
         </div>
-        <div class="box-instalaciones" style=" background: #EC945E"></div>
-        <div class="box-instalaciones" style=" background: #EC945E"></div>
+        <div class="box-instalaciones" style="z-index: 0;position: relative;">
+            <img class="full-size" src="<?php echo get_template_directory_uri();?>/images/instalaciones/areaComun.png" alt="ÁREAS COMUNES">
+            <div class="flecha-izquierda centrar-fecha-izquierda"></div>
+        </div>
+        <div class="box-instalaciones centrar-texto" style=" background: #EC945E">
+            <p> ÁREAS COMUNES</p>
+        </div>
+        <div class="box-instalaciones centrar-texto" style=" background: #FFF">
+            <p><span>SALONES ULÚA Y OLMECA</span></p>
+        </div>
         <div class="box-instalaciones">
-            <div class="full-size img-salones">
-                <div class="shadow">
+            <div class="full-size img-salones" style="z-index: 0;position: relative;">
+                <div class="shadow text-shadow">
                     <p>Más de 3 mil metros cuadrados de espacio comercial. Salones más íntimos.</p> 
                 </div>
                <!--  <img class="full-size" src="<?php echo get_template_directory_uri();?>/images/instalaciones/salones.png" alt="SALONES ULÚA Y OLMECA"> -->
-                
+               <div class="flecha-derecha-blanca centrar-fecha-derecha"></div>
             </div>
         </div>
-        <div class="box-instalaciones" style=" background: #EC945E"></div>
-        <div class="box-instalaciones">
-        <img class="full-size" src="<?php echo get_template_directory_uri();?>/images/instalaciones/localesComerciales.png" alt="LOCALES COMERCIALES">
+        <div class="box-instalaciones centrar-texto" style=" background: #EC945E">
+            <p>LOCALES COMERCIALES</p>
+        </div>
+        <div class="box-instalaciones centrar-texto" style="z-index: 0;position: relative;">
+            <img class="full-size" src="<?php echo get_template_directory_uri();?>/images/instalaciones/localesComerciales.png" alt="LOCALES COMERCIALES">
+            <div class="flecha-derecha centrar-fecha-derecha"></div>
         </div>
     </div>
 </div>
