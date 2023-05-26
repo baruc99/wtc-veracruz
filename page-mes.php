f47555e3fccc336b99a1d70a4f73ed8b2673492b
BARUC ALBERTO GOMEZ SANCHEZ <bagomez@veracruz.gob.mx>
2023-05-25 18:55:19 -0600

    mes completo baruc 25 de mayo

--- page-mes.php
+++ page-mes.php
@@ -8,49 +8,41 @@
                 <p>Eventos sujetos a cambio sin previo aviso.</p>
             </div>
         </div>
-        <div class="row">
-            <div class="col-2" style="height: 200px;">
+    
+        <div class="row mb-5">
+            <div class="col-2 align-Center">
                 <div class="imgMes">
                     <img src="<?php echo get_template_directory_uri();?>/images/mes/calendario.png" alt="centro de trabajo">
                 </div>
-                <div class="card-img-mes">
-                    <img src=<?php echo get_template_directory_uri();?>"" alt="">
-                </div>
             </div>
-            <div class="col-4 align-Center" style="height: 200px;">
-                <a href="">
-                    <div class="card-eventos align-Center">21-24</div>
-                </a>
+            <div class="col-4 align-Center">
+                <div class="card-dias-mes align-Center">21-24</div>
             </div>
-            <div class="col-6" style="height: 200px;" >
+            <div class="col-6 align-start">
                 <div class="text-mes">
-                    <h3> EXPO TLAQUEPARTE </h3>
+                    <h3>EXPO TLAQUEPARTE</h3>
                     <p>Público - Salones Tajín 1 y 2</p>
                 </div>
             </div>
         </div>
         <div class="row">
-            <div class="col-2" style="height: 200px;">
+            <div class="col-2 align-Center">
                 <div class="imgMes">
                     <img src="<?php echo get_template_directory_uri();?>/images/mes/calendario.png" alt="centro de trabajo">
                 </div>
-                <div class="card-img-mes">
-                    <img src=<?php echo get_template_directory_uri();?>"" alt="">
-                </div>
             </div>
-            <div class="col-4" style="height: 200px;">
-                <a href="">
-                    <div class="card-eventos align-Center">22</div>
-                </a>
+            <div class="col-4 align-Center">
+                <div class="card-dias-mes align-Center">22</div>
             </div>
-            <div class="col-6" style="height: 200px;" >
+            <div class="col-6 align-start">
                 <div class="text-mes">
-                    <h3> CONCIERTO MANUEL JOSÉ </h3>
+                    <h3>CONCIERTO MANUEL JOSÉ</h3>
                     <p>Público - Salones ULÚA 3 Y 4</p>
                 </div>
             </div>
         </div>
-    </div>
+        </div>
+        <div class="altura-eventos-fin"></div>
 </div>
 
 <?php get_template_part('orange-line'); ?>
