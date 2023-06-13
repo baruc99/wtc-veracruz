da240790fe759a901c92d55771cd571fad29c9c9
BARUC ALBERTO GOMEZ SANCHEZ <bagomez@veracruz.gob.mx>
2023-06-12 18:57:00 -0600

    cambios responsivos tableta

--- header.php
+++ header.php
@@ -98,15 +98,15 @@
       <div class="wrapper-header">
         <div class="container">
           <div class="row">
-            <div class="col">
+            <div class="col-md">
               <?php if(isset($general_settings['phone']) && !empty($general_settings['phone'])){ ?>
                   <a href="tel:<?php echo $newPhone ?>" >
                     Tel. <?php echo $general_settings['phone']?>
                   </a>
                 <?php }?>
             </div>
-            <div class="col-md-8 col-2"></div>
-            <div class="col alignLogos">
+            <div class="col-md-5 col-2"></div>
+            <div class="col-md alignLogos">
             <?php if(isset($branding['twitter-url']) && !empty($branding['twitter-url'])){ ?>
                 <a href="<?php echo $branding['facebook-url']; ?>" target="_blank">
                   <img class="logo-header" src="<?php echo get_template_directory_uri();?>/images/social-red/twitter-vino.png" alt="logo twiter">
